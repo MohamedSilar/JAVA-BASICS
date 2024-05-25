752d054a1dbbcfff24ae17cb785054a5bdefe0b0
Your Name <you@example.com>
2024-05-25 12:10:23 +0530

    first comit

---
+++ java_start/arms.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class arms{
+    public static void main(String[] args) {
+        Scanner Scan= new Scanner(System.in);
+        int Scan= leo.nextInt();
+        System.out.println("Enter the number is:" ,leo);
+        
+    }
+}
